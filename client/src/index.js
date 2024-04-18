aa19145666eb6bde91221ba34886600f800fec0f
jagadeesh <jagadeeshm2002@gmail.com>
2024-04-18 09:36:59 +0530

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ThemeProvider } from "@material-tailwind/react";
+import { Provider } from 'react-redux';
+import { store } from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
