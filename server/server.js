5b3dc98600b88d2ae0eb0d2b129cabd32d33289e
jagadeesh <jagadeeshm9876@gmail.com>
2024-05-02 18:11:11 +0530

    login routes

--- server/server.js
+++ server/server.js
@@ -15,9 +15,16 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 app.use("/",require("./routes/root"))
+app.use('/auth',require('./routes/authRoutes'))
+app.use("/users",require('./routes/userRoutes'))
 // app.get("/", (req, res) => {
 //     res.send("Hello World!");
 // })
+app.all('*', (req, res) => {
+    res.status(404);
+    res.type('txt').send("no access");
+})
+
 app.use(errorHandler)
 app.listen(PORT, () => {
     console.log("Listening on port ",PORT);
