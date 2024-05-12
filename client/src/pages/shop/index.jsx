003ab8df4c6161ff86aa92ed3989bd89a7b7fac1
jagadeesh <jagadeeshm2002@gmail.com>
2024-05-12 11:45:36 +0530

    shop updated

--- client/src/pages/shop/index.jsx
+++ client/src/pages/shop/index.jsx
@@ -1,16 +1,66 @@
-import React from "react";
-import Price from "../../components/price";
-import Rating from "../../components/rating";
+import React, { useState } from "react";
+import MultiRangeSlider from "../../utilities/multiRangeSlider";
+import {Select,Option} from "@material-tailwind/react";
 
-const Shop = (category) => {
-  console.log(category);
+const Shop = () => {
+  const [priceRange, setPriceRange] = useState([1, 5000]);
+  const[sortBy,setSortBy]=useState("newest")
+
   return (
-    <div className="bg-red-700 w-full flex justify-center">
-      <div className="max-w-screen-xl bg-blue-gray-800">
+    <div className="bg-red-700 w-full flex justify-center py-12">
+      <div className=" max-w-screen-lg w-[960px] bg-blue-gray-600">
         <div className="flex flex-row w-full">
-          <div className="w-1/3"><div><div><Price /></div><div><Rating/></div></div></div>
-          <div className="w-2/3"><div><div>filter</div><div>shop</div></div></div>
-          
+          <aside className="w-1/3 mx-6">
+            <div className="space-y-2">
+              <div className="bg-white border border-gray-400 rounded-md w-56 flex flex-col">
+                <div className="px-4 py-2 text-md bg-blue-gray-100 rounded-t-md">
+                  <p className="font-sans font-bold text-start">Price</p>
+                </div>
+                <hr className="border-gray-500" />
+                <div className="px-7 py-8">
+                  <MultiRangeSlider
+                    defaultValue={priceRange}
+                    onChange={setPriceRange}
+                    max={5000}
+                    className="flex"
+                  />
+                </div>
+              </div>
+              <div className="bg-white border border-gray-400 rounded-md w-56 flex flex-col">
+                <div className="px-4 py-2 text-md bg-blue-gray-100 rounded-t-md">
+                  <p className="font-sans font-bold text-start">Price</p>
+                </div>
+                <hr className="border-gray-500" />
+                <div className="px-7 py-8">
+                  <MultiRangeSlider
+                    defaultValue={priceRange}
+                    onChange={setPriceRange}
+                    max={5000}
+                    className="flex"
+                  />
+                </div>
+              </div>
+            </div>
+          </aside>
+          <section className="w-2/3">
+            <div className="flex flex-col space-y-2 ">
+              <div className="flex flex-row justify-between  items-center border border-gray-600 px-6 py-2 rounded-lg bg-gray-50">
+                <div>
+                  <p>showing 12 of 24 results</p>
+                </div>
+                <div className="flex flex-row justify-around gap-2 items-center">
+                  <p className="font-sans font-bold text-start w-full">Sort by</p>
+
+                  <Select className=" !border !border-gray-300 bg-white text-gray-900 "  labelProps={{className:"hidden"}} value={sortBy} onChange={(val)=>setSortBy(val)}>
+                    <Option  value="newest">Newest First</Option>
+                    <Option value="high" >Price High to Low</Option>
+                    <Option value="low">Price Low to High</Option>
+                  </Select>
+                </div>
+              </div>
+            </div>
+            <div className="text-lg">Shop</div>
+          </section>
         </div>
       </div>
     </div>
--- client/src/utilities/multiRangeSlider/index.jsx
+++ client/src/utilities/multiRangeSlider/index.jsx
@@ -2,32 +2,64 @@ import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import { useState } from "react";
 
-export default ({defaultValue,max,onChange,className}) => {
-  
-    const [rangeValue, setRangeValue] = useState(defaultValue);
-    console.log(rangeValue)
+export default ({ defaultValue, max, onChange, className, slider }) => {
+  const [rangeValue, setRangeValue] = useState(defaultValue);
+  console.log(rangeValue);
 
-  const rangeChange = newValue => {
+  const rangeChange = (newValue) => {
     setRangeValue(newValue);
   };
-  const onRangeChangeComplete = value => {
+  const onRangeChangeComplete = (value) => {
     onChange(value);
   };
 
   return (
     <>
-      <Slider
-        range
-        className={className}
-        min={1}
-        pushable={20}
-        max={max}
-        allowCross={false}
-        value={rangeValue}
-        onChange={rangeChange}
-        onChangeComplete={onRangeChangeComplete}
-      />
+      {slider ? (
+        <>
+          <Slider
+            range
+            className={className}
+            min={1}
+            pushable={20}
+            max={max}
+            allowCross={false}
+            value={rangeValue}
+            onChange={rangeChange}
+            onChangeComplete={onRangeChangeComplete}
+          />
+          <div className="flex justify-between ">
+            <p className="text-sm font-sans font-semibold text-blue-gray-800">
+              ₹{rangeValue[0]}
+            </p>
+            <p className="text-sm font-sans font-semibold text-blue-gray-800">
+              ₹{rangeValue[1] === max ? "5000+" : rangeValue[1]}
+            </p>
+          </div>
+        </>
+      ) : (
+        <>
+          <Slider
+            range
+            className={className}
+            min={1}
+            pushable={20}
+            max={max}
+            allowCross={false}
+            value={rangeValue}
+            onChange={rangeChange}
+            onChangeComplete={onRangeChangeComplete}
+          />
+          <div className="flex justify-between ">
+            <p className="text-sm font-sans font-semibold text-blue-gray-800">
+              ₹{rangeValue[0]}
+            </p>
+            <p className="text-sm font-sans font-semibold text-blue-gray-800">
+              ₹{rangeValue[1] === max ? "5000+" : rangeValue[1]}
+            </p>
+          </div>
+        </>
+      )}
     </>
   );
 };
-
