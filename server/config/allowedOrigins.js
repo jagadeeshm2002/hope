155dfe2afc403d50f4b0d3679de36145a634fecd
jagadeesh <jagadeeshm9876@gmail.com>
2024-05-27 00:18:38 +0530

    cors updated

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,12 @@
 const allowedOrigins =[
     'http://localhost:3000',
     'https://google.com',
-    'http://localhost:3500'
+    'http://localhost:3500',
+    "hopestoreserver-g81cbd1ak-jagadeeshm2002s-projects.vercel.app",
+    "https://hopestoreserver.vercel.app",
+    "https://hopestore-ecommerce.vercel.app/"
+    
+
 ]
 
 module.exports = allowedOrigins;
