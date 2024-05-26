2c09fdf1791cfb15f039f7744f3494607b91d4d3
jagadeesh m <91142244+jagadeeshm2002@users.noreply.github.com>
2024-05-27 01:28:15 +0530

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,9 +2,10 @@ const allowedOrigins =[
     'http://localhost:3000',
     'https://google.com',
     'http://localhost:3500',
-    "hopestoreserver-g81cbd1ak-jagadeeshm2002s-projects.vercel.app",
-    "https://hopestoreserver.vercel.app",
-    "https://hopestore-ecommerce.vercel.app/"
+    "hope-ef53ndupq-jagadeeshm2002s-projects.vercel.app",
+    "hopestore-ecommerce.vercel.app",
+    "hopestore-server.vercel.app",
+    "hope-3ys7-iq2l2067q-jagadeeshm2002s-projects.vercel.app",
     
 
 ]
