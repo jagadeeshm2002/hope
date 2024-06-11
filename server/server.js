a3c315bad377d30cb2afe2a3fc969ae9ed74f820
jagadeesh m <91142244+jagadeeshm2002@users.noreply.github.com>
2024-06-11 12:23:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const mongoose =require('mongoose');
 
 connectDB() 
 const PORT = process.env.PORT || 3500
-app.use(logger)
+// app.use(logger)
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
