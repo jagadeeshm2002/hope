3e383aa2ddbaf5dfe4b406b7a4cb756331ed2a3e
jagadeesh m <jagadeeshm2002@gmail.com>
2024-09-16 13:37:53 +0530

    seperate the url for backend server to env file

--- client/src/api/apiSlice.js
+++ client/src/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logOut } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://hopestore-server.vercel.app/',
+    baseUrl: process.env.REACT_APP_BASE_URL,
     credentials: 'include',
     
     prepareHeaders: (headers, { getState }) => {
