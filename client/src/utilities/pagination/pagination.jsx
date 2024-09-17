24411d6dcf2314b01e0915252ae58fa940c1f5c9
jagadeesh m <jagadeeshm2002@gmail.com>
2024-09-17 14:21:46 +0530

    fixes all the errors

--- client/src/utilities/pagination/pagination.jsx
+++ client/src/utilities/pagination/pagination.jsx
@@ -1,8 +1,7 @@
-
 import { IconButton, Typography } from "@material-tailwind/react";
 import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
 
-export function Pagination({ active, setActive, total,error }) {
+export function Pagination({ active, setActive, total, error }) {
   const next = () => {
     if (active === total) return;
 
@@ -16,28 +15,24 @@ export function Pagination({ active, setActive, total,error }) {
   };
 
   return (
-    <div className={`flex items-center gap-8 `}  >
+    <div className={`flex items-center gap-8 `}>
       <IconButton
         size="sm"
         variant="outlined"
         onClick={prev}
-        disabled={error ?true:active === 1}
-        
+        disabled={error ? true : active === 1}
       >
         <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" />
       </IconButton>
       <Typography color="gray" className="font-normal">
-        
         <strong className="text-gray-900">{active}</strong> of{" "}
-        <strong className="text-gray-900">{total||1}</strong>
+        <strong className="text-gray-900">{total || 1}</strong>
       </Typography>
       <IconButton
         size="sm"
         variant="outlined"
         onClick={next}
-        disabled={error ?true: active === 10 }
-       
-
+        disabled={error ? true : active === 10}
       >
         <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
       </IconButton>
