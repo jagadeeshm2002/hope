45dfc009bcc3605d2b915886a35c1cc8e6dcef4e
jagadeesh m <jagadeeshm2002@gmail.com>
2024-09-17 12:46:33 +0530

    seperated componet brand filter

---
+++ client/src/components/filter/BrandFilter.jsx
@@ -0,0 +1,17 @@
+import { Radio} from '@material-tailwind/react';
+import React from 'react'
+
+function BrandFilter() {
+    const brandList=["hope"]
+  return (
+    <div className='w-full my-3 px-1  '>
+      <p className="font-sans font-bold text-start pl-3">Brands</p>
+      <div className="flex flex-col">
+        {brandList.map((brand) => (
+          <Radio className="flex flex-row justify-between items-center my-1" key={brand} name={brand} label={brand} defaultChecked={brand === "hope" && true}  disabled/> ))}
+      </div>
+    </div>
+  )
+}
+
+export default BrandFilter
