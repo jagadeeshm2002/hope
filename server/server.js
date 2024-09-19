caf749e457e53e9bd789219eb68b3cb9ecf2714a
jagadeesh m <jagadeeshm2002@gmail.com>
2024-09-19 21:58:05 +0530

    webhook add

--- server/server.js
+++ server/server.js
@@ -25,6 +25,26 @@ app.use('/auth',require('./routes/authRoutes'))
 app.use("/users",require('./routes/userRoutes'))
 app.use('/products',require('./routes/productRoutes'))
 app.use('/cart',require('./routes/cartRoutes'))
+app.post('/webhook', (req, res) => {
+    const event = req.headers['x-github-event'];
+    const payload = req.body;
+
+    if (event === 'pull_request') {
+        const action = payload.action;
+        const pullRequest = payload.pull_request;
+
+        console.log(`Pull Request ${action}: #${pullRequest.number} by ${pullRequest.user.login}`);
+
+        // Check if the pull request body contains a bounty command
+        if (pullRequest.body.includes('/bounty')) {
+            console.log('Bounty found:', pullRequest.body);
+            // Extract bounty information and process it here
+        }
+    }
+
+    res.status(200).send('Webhook received');
+});
+
 // app.get("/", (req, res) => {
 //     res.send("Hello World!");
 // })
